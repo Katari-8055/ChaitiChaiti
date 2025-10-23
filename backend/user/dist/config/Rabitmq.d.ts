c135ba5cee39a0f21dc5b3efe1df218a3836bc3a
Katari <kataricoder@gmail.com>
2025-10-24 01:54:27 +0530

    intial commit of my project

---
+++ backend/user/dist/config/Rabitmq.d.ts
@@ -0,0 +1,3 @@
+export declare const connectToRabbitMQ: () => Promise<void>;
+export declare const publishToQueue: (queueName: string, message: any) => Promise<void>;
+//# sourceMappingURL=Rabitmq.d.ts.map
---
+++ backend/user/src/index.ts
@@ -0,0 +1,41 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import connetDB from './config/DB.js';
+import { createClient } from 'redis';
+import userRoutes from './routes/user.js';
+import { connectToRabbitMQ } from './config/Rabitmq.js';
+
+dotenv.config();
+
+connetDB();
+
+connectToRabbitMQ();
+
+
+
+const redisUrl = process.env.REDIS_URL;
+if (!redisUrl) {
+  throw new Error("REDIS_URL is not defined in .env");
+}
+
+export const redisClient = createClient({
+  url: redisUrl, 
+});
+
+redisClient.connect().then(() => {
+    console.log("Connected to Redis");
+}).catch((err) => {
+    console.log(err);
+    process.exit(1);
+})
+
+
+
+const app = express();
+
+app.use("/api/v1", userRoutes);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`User service is running on port ${port}`);
+})
